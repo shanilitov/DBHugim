37eb5b34e49eec6955360d5a94d21e6ece0001cb
Shani Litov <116593341+shanilitov@users.noreply.github.com>
2024-07-24 11:44:57 +0300

    view1

---
+++ Stage4/view.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE VIEW GROUPDETAILS AS
+SELECT
+    g.idGroup AS GroupID,
+    COUNT(kig.idKid) AS NumberOfKids,
+    t.name AS TeacherName,
+    s.sday AS ScheduleDay,
+    s.shour AS ScheduleHour
+FROM
+    Course_Group g
+    LEFT JOIN Kid_In_Group kig ON g.idGroup = kig.idGroup
+    LEFT JOIN group_in_schedulle gis ON g.idGroup = gis.idGroup
+    LEFT JOIN Schedule s ON gis.idSchedule = s.idSchedule
+    LEFT JOIN Teacher_In_Schedule tis ON s.idSchedule = tis.idSchedule
+    LEFT JOIN Teachers t ON tis.idTeacher = t.idTeacher
+GROUP BY
+    g.idGroup,
+    t.name,
+    s.sday,
+    s.shour;
